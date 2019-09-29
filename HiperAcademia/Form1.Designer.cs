175eaea48ae9027f3c6cdc809e06f586f59a0d46
Adriano Dell Agnolo <adriano@hiper.com.br>
2019-09-29 10:55:32 -0300

    Commit

---
+++ HiperAcademia/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace HiperAcademia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cbxDia = new System.Windows.Forms.ComboBox();
+            this.cbxMes = new System.Windows.Forms.ComboBox();
+            this.cbxAno = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(255, 86);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(349, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(255, 140);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(255, 167);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nascimento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Peso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Altura";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(255, 218);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Caluclar IIMC";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // cbxDia
+            // 
+            this.cbxDia.FormattingEnabled = true;
+            this.cbxDia.Location = new System.Drawing.Point(255, 111);
+            this.cbxDia.Name = "cbxDia";
+            this.cbxDia.Size = new System.Drawing.Size(57, 21);
+            this.cbxDia.TabIndex = 9;
+            // 
+            // cbxMes
+            // 
+            this.cbxMes.FormattingEnabled = true;
+            this.cbxMes.Location = new System.Drawing.Point(318, 111);
+            this.cbxMes.Name = "cbxMes";
+            this.cbxMes.Size = new System.Drawing.Size(124, 21);
+            this.cbxMes.TabIndex = 10;
+            // 
+            // cbxAno
+            // 
+            this.cbxAno.FormattingEnabled = true;
+            this.cbxAno.Location = new System.Drawing.Point(448, 111);
+            this.cbxAno.Name = "cbxAno";
+            this.cbxAno.Size = new System.Drawing.Size(98, 21);
+            this.cbxAno.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbxAno);
+            this.Controls.Add(this.cbxMes);
+            this.Controls.Add(this.cbxDia);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cbxDia;
+        private System.Windows.Forms.ComboBox cbxMes;
+        private System.Windows.Forms.ComboBox cbxAno;
+    }
+}
+
---
+++ HiperAcademia/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiperAcademia
+{
+    public partial class Form1 : Form
+    {
+        string Nome;
+        int Idade;
+        decimal Altura;
+        decimal Peso;
+        int[] Dias;
+        List<int> Anos;
+        Dictionary<int, string> Meses;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Dias = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
+            Anos = new List<int>()
+            {
+                1970,
+                1980,
+                1981,
+                1982,
+                1983,
+                1984,
+                1995,
+                1996,
+                1997,
+                1998,
+                1999,
+                2000
+            };
+            
+            Meses = new Dictionary<int, string>()
+            {
+                { 1 , "Janeiro" },
+                { 2 , "Fevereiro" },
+                { 3 , "Março" },
+                { 4 , "Abril" },
+                { 5 , "Maio" },
+                { 6 , "Junho" },
+                { 7 , "Julho" },
+                { 8 , "Agosto" },
+                { 9 , "Setembro" },
+                { 10 , "Outubro" },
+                { 11 , "Novembro" },
+                { 12 , "Dezembro" }
+            };
+
+            foreach (int dia in Dias)
+            {
+                cbxDia.Items.Add(dia);
+            }
+
+            foreach (int ano in Anos)
+            {
+                cbxAno.Items.Add(ano);
+            }
+
+            cbxMes.DataSource = new BindingSource(Meses, null);
+            cbxMes.DisplayMember = "Value";
+            cbxMes.ValueMember = "Key";
+
+            
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            string Nome = txtNome.Text;            
+
+            if (!decimal.TryParse(txtAltura.Text, out Altura))
+            {
+                MessageBox.Show("Altura inválida!");
+                return;
+            }
+
+            if (!decimal.TryParse(txtPeso.Text, out Peso))
+            {
+                MessageBox.Show("Peso inválida!");
+                return;
+            }
+
+            string descricao = VerificarImc(Peso, Altura, out decimal imc);
+            
+            MessageBox.Show($"Nome: {Nome}\nNascimento: {cbxDia.Text} de {cbxMes.Text} de {cbxAno.Text}\nIMC: {imc.ToString("N2")}\n\n{descricao}");
+        }
+
+        private string VerificarImc(decimal peso, decimal altura, out decimal imc)
+        {
+            imc = Peso / (Altura * Altura);
+
+            if (imc < (decimal)18.5)
+            {
+                return "Abaixo do peso";
+            }
+            else if (imc >= (decimal)18.5 && imc < 25)
+            {
+                return "Peso normal";
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                return "Sobrepeso";
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                return "Obesidade grau 1";
+            }
+            else if (imc >= 35 && imc < 39)
+            {
+                return "Obesidade grau 2";
+            }
+            else
+            {
+                return "Obesidade grau 3";
+            }
+        }
+    }
+}
